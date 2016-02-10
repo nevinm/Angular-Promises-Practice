621b9f574231a5a6c5faf9573d622a0b90270ff7
Nevin Madhukar K <nevinm@qburst.com>
2016-02-10 23:27:23 +0530

    Added Basic Q check

--- app/js/main/controllers/HomeCtrl.js
+++ app/js/main/controllers/HomeCtrl.js
@@ -1,3 +1,20 @@
-app.controller('HomeCtrl', ['$scope', function($scope) {
-  
+app.controller('HomeCtrl', ['$scope', '$q', function($scope, $q) {
+    $scope.checkNumberDefer = function(number) {
+        var q = $q.defer();
+        if (angular.isNumber(number)) {
+            q.resolve("Yo this shit is right!")
+        } else {
+            q.reject("NAN");
+        }
+        return q.promise;
+    }
+
+    $scope.value = "0";
+    $scope.numberGucciCheck =  $scope.checkNumberDefer($scope.value);
+
+    $scope.numberGucciCheck.then(function(weGood) {
+        $scope.status = weGood;
+    }, function(err) {
+        $scope.status = err;
+    });
 }]);
